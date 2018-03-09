db484ea6ff886dae82e6975c90a955babb419706
hcz <h.cz@qq.com>
2018-03-09 02:36:14 -0500

    implement a simple markdown parser

--- gen_readme.py
+++ gen_readme.py
@@ -1,24 +1,96 @@
 #! /usr/bin/env python3
 
-from textwrap import dedent
-from pathlib import Path
+import sys
+import re
+import textwrap
+import datetime
+import pathlib
 
 INDENT_WIDTH = 4
 
-def print_path(path, indent):
-    print(' ' * indent * INDENT_WIDTH, end='')
-    print('* [%s](%s)' % (path.name, '/'.join(path.parts)))
+def print_item(content, indent):
+    sys.stdout.write('%s* %s\n' % (' ' * INDENT_WIDTH * indent, content))
 
-def print_dir(path, indent):
-    for sub_path in sorted(path.iterdir()):
-        if sub_path.is_dir():
-            print_path(sub_path, indent)
-            print_dir(sub_path, indent + 1)
-        elif sub_path.is_file() and not sub_path.match('README.md'):
-            print_path(sub_path, indent)
+def print_link(path, title, indent):
+    print_item('[%s](%s)' % (title, '/'.join(path.parts)), indent)
+
+def process_file_item(path, used_files, name, title):
+    with path / name as file_item:
+        assert file_item.exists()
+        assert file_item.is_file()
+
+        used_files.append(name)
+
+        print_link(file_item, title, 2)
+
+def process_readme(path, used_files, time_str):
+    with path / 'README.md' as readme:
+        assert readme.exists()
+        assert readme.is_file()
+
+        used_files.append('README.md')
+
+        lines = readme.open().read().splitlines()
+        assert len(lines) >= 2
+        assert lines[0].endswith(' - ' + time_str)
+        assert lines[1] == '==='
+
+        last = None
+        section = None
+
+        for line in lines:
+            if line == '---':
+                assert last is not None
+
+                print_item(last, 1)
+                section = last
+            elif re.match(r'^\[.*\]\((?!\w+://).*\)$', line):
+                assert section is not None
+
+                match = re.match(r'^\[(.+)\]\((.+)\)$', line)
+                assert match is not None
+
+                process_file_item(
+                    path,
+                    used_files,
+                    match.group(2),
+                    match.group(1)
+                )
+
+            last = line
+
+def process_slides(path, used_files):
+    with path / 'SLIDES.pdf' as slides:
+        if slides.exists():
+            assert slides.is_file()
+
+            used_files.append('SLIDES.pdf')
+
+            print_link(slides, 'Slides', 2)
+
+def process_dir(path):
+    if path.match('????-??-??'):
+        assert path.is_dir()
+
+        time = datetime.datetime.strptime(path.name, '%Y-%m-%d')
+        time_str = time.strftime('%b %d, %Y')
+        print_link(path, time_str, 0)
+
+        used_files = []
+
+        process_readme(path, used_files, time_str)
+        process_slides(path, used_files)
+
+        assert sorted(used_files) == sorted([
+            file_item.name for file_item in path.iterdir()
+        ])
+
+def process_all(root):
+    for path in sorted(root.iterdir()):
+        process_dir(path)
 
 if __name__ == '__main__':
-    print(dedent('''\
+    sys.stdout.write(textwrap.dedent('''\
         Weekly Events
         ===
 
@@ -26,18 +98,16 @@ if __name__ == '__main__':
 
         Table of Contents
         ---
+
     '''))
 
-    for path in sorted(Path('.').iterdir()):
-        if path.is_dir() and path.match('????-??-??'):
-            print_path(path, 0)
-            print_dir(path, 1)
+    process_all(pathlib.Path('.'))
 
-    print(dedent('''
+    sys.stdout.write(textwrap.dedent('''
         Contribution
         ---
 
         If you are interested in doing a presentation in our weekly events, please upload slides or an outline document by pushing a commit or opening a pull request.
 
         PDF and markdown are suggested formats.
-    '''), end='')
+    '''))
